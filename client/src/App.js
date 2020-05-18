6ed95bf7064da3e2d0584c779a51a39afb5dd751
christopher rees <crees979@gmail.com>
2020-05-18 16:57:28 -0600

    getting pic

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Home from './Home'
+import Tweets from "./Tweets"
+import Profile from './Profile';
+import {Switch, Route, Link} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+      <Link to="/">Home</Link>
+          <Link to="/Tweets">tweets</Link>
+          <Link to="/Profile">Profile</Link>
+      </nav>
+    <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+          <Route path="/Tweets">
+              <Tweets/>
+          </Route>
+        <Route path="/Profile">
+          <Profile/>
+        </Route>
+      </Switch>
     </div>
   );
 }
